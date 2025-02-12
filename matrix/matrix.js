1068fb39dc15ddbe07d3a36ba7d4607583ee312c
Birasa Divine <b.divine@alustudent.com>
2025-02-12 22:43:42 +0200

    Matrix

---
+++ matrix/matrix.js
@@ -0,0 +1,8 @@
+class sparseMatrix {
+  constructor(numRows, numCols) {
+    this.numCols = numRows;
+    this.numCols = numCols;
+    this.data = new Map();
+  }
+  static fromFile(fileContent) {}
+}
