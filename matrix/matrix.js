13aeedbc60ecca4e6dff9af675ec291f14fd3a8e
Birasa Divine <b.divine@alustudent.com>
2025-02-19 23:04:26 +0200

    Delete matrix directory

--- matrix/matrix.js
+++
@@ -1,8 +0,0 @@
-class sparseMatrix {
-  constructor(numRows, numCols) {
-    this.numCols = numRows;
-    this.numCols = numCols;
-    this.data = new Map();
-  }
-  static fromFile(fileContent) {}
-}
